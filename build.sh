89476c7a67c356599bd4db76e46f5223f1da9af9
Wright <guan1992@gmail.com>
2020-10-11 23:23:33 +0800

    ci: change build script file

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
+# run all unit test
+go test ./... -v -count=1 -failfast 
+
+# binding env 
 export CGO_ENABLED=0
 export GOOS=linux
 export GOARCH=amd64
+
+# build binary file
 go build -o bin/gintest cmd/gintest/main.go 
-go build -o bin/migrate cmd/gintest/migrate.go
+go build -o bin/migrate cmd/migrate/main.go
+
+# build docker image
 docker build --build-arg ENV=example -t=wright1992/gintest:latest .
